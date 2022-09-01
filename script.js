1fb1d842b1395997187eeb047d2dd73912894032
Santhoshkumar <110901125+santajoyal@users.noreply.github.com>
2022-09-01 14:52:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function foo(){
+var details=document.getElementById("fn").value;
+console.log(details);
+var details=document.getElementById("mn").value;
+console.log(details);
+var details=document.getElementById("ln").value;
+console.log(details);
+var details=document.getElementById("em").value;
+console.log(details);
+
+}
